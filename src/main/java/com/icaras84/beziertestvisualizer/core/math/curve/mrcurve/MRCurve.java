ae764301a3d6d99a80f728d60ec2399383d78d01
William <icaras84@gmail.com>
2026-06-19 22:39:46 -0700

    switched MRCurve to a record class

--- src/main/java/com/icaras84/beziertestvisualizer/core/math/curve/mrcurve/MRCurve.java
+++ src/main/java/com/icaras84/beziertestvisualizer/core/math/curve/mrcurve/MRCurve.java
@@ -5,41 +5,7 @@ import com.icaras84.beziertestvisualizer.core.math.curve.Curve;
 import org.ejml.data.DMatrixRMaj;
 import org.ejml.dense.row.CommonOps_DDRM;
 
-public class MRCurve implements Curve {
-
-    private DMatrixRMaj controlMatrix;
-    private DMatrixRMaj basisMatrix;
-    private PolynomialMatrix polynomialMatrix;
-
-    public MRCurve(DMatrixRMaj controlMatrix, DMatrixRMaj basisMatrix, PolynomialMatrix polynomialMatrix) {
-        this.controlMatrix = controlMatrix;
-        this.basisMatrix = basisMatrix;
-        this.polynomialMatrix = polynomialMatrix;
-    }
-
-    public DMatrixRMaj getControlMatrix() {
-        return controlMatrix;
-    }
-
-    public void setControlMatrix(DMatrixRMaj controlMatrix) {
-        this.controlMatrix = controlMatrix;
-    }
-
-    public DMatrixRMaj getBasisMatrix() {
-        return basisMatrix;
-    }
-
-    public void setBasisMatrix(DMatrixRMaj basisMatrix) {
-        this.basisMatrix = basisMatrix;
-    }
-
-    public PolynomialMatrix getPolynomialMatrix() {
-        return polynomialMatrix;
-    }
-
-    public void setPolynomialMatrix(PolynomialMatrix polynomialMatrix) {
-        this.polynomialMatrix = polynomialMatrix;
-    }
+public record MRCurve(DMatrixRMaj controlMatrix, DMatrixRMaj basisMatrix, PolynomialMatrix polynomialMatrix) implements Curve {
 
     public DMatrixRMaj premultipliedBasisAndControl() {
         return CommonOps_DDRM.mult(this.basisMatrix, this.controlMatrix, null);
@@ -49,28 +15,28 @@ public class MRCurve implements Curve {
     public Complex getPosition(double t) {
         DMatrixRMaj output = new DMatrixRMaj();
         CommonOps_DDRM.mult(this.polynomialMatrix.getTMatrix(0, t), this.premultipliedBasisAndControl(), output);
-        return new Complex(output.data[0],  output.data[1]);
+        return new Complex(output.data[0], output.data[1]);
     }
 
     @Override
     public Complex getTangent(double t) {
         DMatrixRMaj output = new DMatrixRMaj();
         CommonOps_DDRM.mult(this.polynomialMatrix.getTMatrix(1, t), this.premultipliedBasisAndControl(), output);
-        return new Complex(output.data[0],  output.data[1]);
+        return new Complex(output.data[0], output.data[1]);
     }
 
     @Override
     public Complex getTangentGradient(double t) {
         DMatrixRMaj output = new DMatrixRMaj();
         CommonOps_DDRM.mult(this.polynomialMatrix.getTMatrix(2, t), this.premultipliedBasisAndControl(), output);
-        return new Complex(output.data[0],  output.data[1]);
+        return new Complex(output.data[0], output.data[1]);
     }
 
     @Override
     public double getCurvature(double t) {
         Complex vel = this.getTangent(t);
         Complex accel = this.getTangentGradient(t);
-        double numerator = vel.getReal() * accel.getImag() -  vel.getImag() * accel.getReal();
+        double numerator = vel.getReal() * accel.getImag() - vel.getImag() * accel.getReal();
         if (numerator == 0.0) {
             return 0.0;
         }
@@ -118,7 +84,7 @@ public class MRCurve implements Curve {
         for (int i = 0; i < tValues.length; i++) {
             int xIdx = 2 * i;
             int yIdx = xIdx + 1;
-            output.data[i] = curvature(velocities.data[xIdx],  velocities.data[yIdx], accelerations.data[xIdx], accelerations.data[yIdx]);
+            output.data[i] = curvature(velocities.data[xIdx], velocities.data[yIdx], accelerations.data[xIdx], accelerations.data[yIdx]);
         }
 
         return output;
